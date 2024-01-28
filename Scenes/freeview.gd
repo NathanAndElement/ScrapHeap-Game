2f71968cadb203a219b3cb9d5309b159c4d3a931
NathanCarter-Dev <nathan.carter3252@gmail.com>
2024-01-28 16:52:13 +0000

    Made debug camera have a zoom

--- Scenes/freeview.gd
+++ Scenes/freeview.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 200.0
+@onready var camera = $Camera2D
 
 func  _physics_process(delta):
 	#Player movement
 	var input_direction =  Vector2(
 	Input.get_action_strength("right") - Input.get_action_strength("left"),
 	Input.get_action_strength("down") - Input.get_action_strength("up"))
+	
+	if Input.get_action_strength("Camera Zoom In"):
+		camera.zoom.x += 0.001
+		camera.zoom.y += 0.001
+	if Input.get_action_strength("Camera Zoom Out"):
+		camera.zoom.x -= 0.001
+		camera.zoom.y -= 0.001
 	
 	velocity = input_direction * speed
 	move_and_slide();
