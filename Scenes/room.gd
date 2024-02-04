4b79fabd0e7eb2ea39f8927f6bd200bf2050615c
NathanCarter-Dev <nathan.carter3252@gmail.com>
2024-02-04 16:07:44 +0000

    Tweaked generation

--- Scenes/room.gd
+++ Scenes/room.gd
@@ -5,4 +5,22 @@ extends Node
 	Vector2(-1,0): true,  # Left door
 	Vector2(0,1): true,   # Bottom door
 	Vector2(0,-1): true   # Top door
-  }
+}
+
+var door_scene = preload("res://Scenes/door.tscn")
+
+func _enter_tree():
+	var offset_distance = 240  # Adjust based on your door size or desired distance from the center
+	for door in door_states:
+		if door_states[door]:
+			var new_door = door_scene.instantiate()
+			var offset = door * offset_distance  # Calculate offset based on direction
+			new_door.position = self.position + offset  # Apply offset to position
+			new_door.name = str(door)
+			add_child(new_door)  # Add the new door as a child of the current node
+			
+
+func delete_unused_doors(doors):
+	for door in doors:
+		print(is_inside_tree())
+		remove_child(get_node(str(door)))
--- Scripts/dungeon_generation.gd
+++ Scripts/dungeon_generation.gd
@@ -17,9 +17,10 @@ func generate(room_seed):
 	dungeon[Vector2(0,0)] = room.instantiate()
 	size -= 1
 	
-
-	
 	while(size > 0):
+		if(size <= 1):
+			delete_unused_doors(dungeon)
+		
 		for i in dungeon.keys():
 			if(randf_range(0, 100) < generation_chance):
 				var direction = randf_range(0, 4)
@@ -41,8 +42,12 @@ func create_rooms(dungeon, direction, i):
 	var new_room_position = i + direction
 	if(!dungeon.has(new_room_position) and dungeon[i].is_door_enabled(direction)):
 		dungeon[new_room_position] = room.instantiate()
-		connect_rooms(dungeon.get(i), dungeon.get(new_room_position), direction)
-		return true
+		if (dungeon[new_room_position].is_door_enabled(-direction)):
+			connect_rooms(dungeon.get(i), dungeon.get(new_room_position), direction)
+			return true
+		else:
+			dungeon.erase(new_room_position)
+			return false
 	return false	
 					
 func connect_rooms(room1, room2, direction):
@@ -50,3 +55,12 @@ func connect_rooms(room1, room2, direction):
 		room2.connected_rooms[-direction] = room1
 		room1.number_of_connections += 1
 		room2.number_of_connections += 1
+		
+func delete_unused_doors(dungeon):
+	for room_id in dungeon.keys():
+		var doors_to_delete = []
+		var room = dungeon[room_id]
+		for door in room.connected_rooms:
+			if(dungeon[room_id].connected_rooms[door] == null):
+				doors_to_delete.append(door)
+		dungeon[room_id].room.delete_unused_doors(doors_to_delete)
